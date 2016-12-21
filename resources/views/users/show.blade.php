d35bc60bf8f2d892e1e83db686b39e3bb378f9ab
Mariusz Franciszczak <mf1969@gmail.com>
2016-12-21 23:23:14 +0100

    Trzeci commit 201216

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,39 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
+<div class="container">
+    <div class="row">
 
-            @include('layouts.sidebar')
-
-            <div class="col-md-9 ">
+        @include('layouts.sidebar')
+    
+        <div class="col-md-7">
+            
+            @if (auth()->check())
                 <div class="panel panel-default">
-
-
                     <div class="panel-body">
-                        <form method="POST" action='{{url('posts/')}}'>
-                            {{ csrf_field() }}
-                            <textarea name='post_content' class='form-control' rows="5" cols="30"
-                                      placeholder="Co słychać  ?" style="margin-bottom: .5rem"></textarea>
-                            <button type='submit' class='btn btn-default pull-right'>Zapisz</button>
-                        </form>
+                        @include('posts.create')
                     </div>
                 </div>
-                @foreach($posts as $post)
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <img class="img-responsive marginesy"  src="{{url('users-avatar/'.$user->id.'/60')}}" alt="avatar"/>
-                            {{$post->user->name}}
-                            {{$post->created_ad}}
-                        </div>
+            @endif
+            @foreach($posts as $post)
+                @include('posts.single')
+            @endforeach
 
-                        <div class="panel-body">
-                            {{$post->content}}
-                        </div>
-                    </div>
-                @endforeach
-            </div>
         </div>
-    </div>
-@endsection
 
+    </div>
+</div>
+@endsection
